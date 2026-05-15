8f2365b93d2ebad7d67fddbae06bbc26a6c3015b
luevb <luevboris1345@gmail.com>
2026-05-15 23:55:58 +0300

    add export to Excel

---
+++ wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿window.saveAsFile = function (filename, base64) {
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = 'data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,' + base64;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
